ecaa65b3d084984552d50c610a843f1d81bcc45a
Viktor Zinchenko <127389112+org-ssh-2@users.noreply.github.com>
2023-05-26 15:17:22 +0800

    Update and rename page-2.js to online-chart-maker.js

---
+++ src/pages/online-chart-maker.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const SecondPage = () => (
+<Layout>
+  <SEO title="Online Chart Makers" keywords={[`online chart makers`, `charts`, `graphs`, `web-based applications`, `templates`, `customizations`, `cloud-based`]} />
+  <h1>Online Chart Makers</h1>
+  <p>Chart making has become much easier thanks to the Internet and online chart makers. These tools allow individuals and businesses to create professional-looking charts in just a few clicks without the need for specialized software or design skills. In this article, we will discuss what online chart makers are, their benefits, and provide a list of the best online chart makers available.</p>
+  <h2>What are online chart makers?</h2>
+  <p>Online chart makers are web-based applications that allow users to create charts and graphs without downloading or installing software. They provide templates, graphs, and customizations for the user to tweak and create various charts that can be used for presentations, reports, and other documentations.</p>
+  <p>They come in both paid and free versions, with some offering more features than others.</p>
+  <h2>Benefits of using online chart makers</h2>
+  <ol>
+    <li>Easy to Use: Online chart makers are easy to use thanks to their intuitive interfaces. Most of these applications provide a drag-and-drop design option, and users can customize their charts without the need for coding or design skills.</li>
+    <li>Saves Time: With online chart makers, you don't have to spend hours creating charts from scratch or looking for the right data points to include. The tools have pre-built templates and customizations that allow users to make charts quickly and efficiently.</li>
+    <li>Affordable: Some online chart makers provide free versions with basic features, while others can be used on a subscription basis. Either way, they are more affordable than hiring a professional to create charts or purchasing expensive software.</li>
+    <li>Cloud-based: Online chart makers are cloud-based, allowing users to access their charts from anywhere. This makes it easy to share charts with colleagues and team members, and collaborate on new charts in real-time.</li>
+  </ol>
+  <h2>Best Online Chart Makers</h2>
+  <ol>
+    <li>Canva: Canva is a well-known graphic design application, and it offers a range of chart-making tools. It has a user-friendly interface and provides a range of templates, graphs, and customizations for creating professional charts.</li>
+    <li>Visme: Visme is a cloud-based platform that provides a range of design options. It offers a range of templates, graphs, and customization options, including animations, interactive charts, and data visualization.</li>
+    <li>Google Sheets: Google Sheets are well-known for their spreadsheets, but they also provide a range of chart-making tools. The platform is cloud-based and can be accessed from anywhere. It provides a range of chart types, including line, pie, bar, and scatter.</li>
+    <li>Plotly: Plotly provides a range of customized chart-making tools. It specializes in interactive and dynamic charts and integrates with a range of programming languages, including Python, R, and Excel.</li>
+    <li>Infogram: Infogram is a well-known platform for creating infographics and charts. It has a user-friendly drag-and-drop interface and provides a range of templates and customizations.</li>
+  </ol>
+  <h2>Conclusion</h2>
+  <p>Online chart makers provide an easy and affordable way of creating professional-looking charts. They offer customization options and are cloud-based, allowing easy collaboration and sharing. With a range of options available, individuals and businesses can find the perfect chart maker that best suits their needs.</p>
+<ul>
+  <li><a href="https://docs.kanaries.net/charts/area-chart-maker">Online Area Chart Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/bar-chart-maker">Online Bar Chart Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/box-plot-maker">Online Box Plot Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/bubble-chart-maker">Online Bubble Chart Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/heatmap-maker">Online Heatmap Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/histogram-maker">Online Histogram Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/line-chart-maker">Online Line Chart Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/pie-chart-maker">Online Pie Chart Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/scatter-plot-maker">Online Scatter Plot Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/box-and-whisker-plot-calculator">Box and Whisker Plot Calculator</a></li>
+  <li><a href="https://docs.kanaries.net/charts/box-and-whisker-plot-maker">Box and Whisker Plot Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/box-plot-calculator">Box Plot Calculator</a></li>
+  <li><a href="https://docs.kanaries.net/charts/box-plot-with-labels">Create Box Plot with Labels</a></li>
+  <li><a href="https://docs.kanaries.net/charts/segmented-bar-graph">Create Segmented Bar Graph</a></li>
+  <li><a href="https://docs.kanaries.net/charts/dot-plot-maker">Dot Plot Maker</a></li>
+  <li><a href="https://docs.kanaries.net/charts/scatter-plot-calculator">Scatter Plot Calculator</a></li>
+  <li><a href="https://docs.kanaries.net/charts/which-tools-would-you-use-to-make-chart">Which Tools Would You Use to Make Chart?</a></li>
+</ul>
+
+<h3>Tips to Create Charts:</h3>
+<ul>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-line-chart-excel">How to Make a Line Chart in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-line-chart-google-sheets">How to Make a Line Chart in Google Sheets</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-a-pivot-chart-in-excel">How to Make a Pivot Chart in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-bubble-charts-excel">How to Create Bubble Charts in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-pie-chart-word">How to Create Pie Chart in Word</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-pie-chart-google-sheet">How to Create Pie Chart in Google Sheets</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-a-pie-chart-in-google-slides">How to Make a Pie Chart in Google Slides</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-a-pie-chart-in-illustrator">How to Make a Pie Chart in Illustrator</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-a-pie-chart-in-word">How to Make a Pie Chart in Word</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-pie-chart-r">How to Make a Pie Chart in R</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-a-stacked-bar-chart">How to Make a Stacked Bar Chart</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-stacked-column-chart-excel">How to Create Stacked Column Chart in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-pie-chart-in-excel">How to Make Pie Chart in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-chart-in-google-docs">How to Make Chart in Google Docs</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-pie-chart-in-google-sheets">How to Make Pie Chart in Google Sheets</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-pie-chart-in-powerpoint">How to Make Pie Chart in PowerPoint</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-pivot-chart-in-excel">How to Make Pivot Chart in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-a-bar-graph-in-excel">How to Create a Bar Graph in Excel</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-a-graph-in-google-docs">How to Create a Graph in Google Docs</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-create-a-line-graph-in-google-sheets">How to Create a Line Graph in Google Sheets</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-a-bar-graph-in-google-sheets">How to Make a Bar Graph in Google Sheets</a></li>
+  <li><a href="https://docs.kanaries.net/charts/how-to-make-charts-in-notion">How to Make Charts in Notion</a></li>
+</ul>
+</Layout>
+)
+
+export default SecondPage
